e35ab696ea24a47747935592fe381d92eee2861d
Hendrik Kao <seemus790@hotmail.com>
2023-08-03 21:53:00 +0200

    Add "BASE_PATH" env variable

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,22 @@
-/** @type {import('vite').UserConfig} */
-export default {
-  base: "/nql-web-formatter/",
-  build: {
-    outDir: "docs",
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          nqlWorker: ["./src/language/nql/nql.worker.ts"],
-          editorWorker: ["monaco-editor/esm/vs/editor/editor.worker"],
+import { defineConfig, loadEnv } from "vite";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: env.BASE_PATH,
+    define: {
+      __BASE_PATH__: JSON.stringify(env.VITE_BASE_PATH),
+    },
+    build: {
+      outDir: "docs",
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            editorWorker: ["monaco-editor/esm/vs/editor/editor.worker"],
+          },
         },
       },
     },
-  },
-};
+  };
+});
