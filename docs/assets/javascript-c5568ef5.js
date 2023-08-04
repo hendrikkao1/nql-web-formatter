fd118a431aaa630245e866b53d4b8aaeb7558317
Hendrik Kao <seemus790@hotmail.com>
2023-08-04 07:12:56 +0200

    Run production build

--- docs/assets/javascript-c5568ef5.js
+++ docs/assets/javascript-c5568ef5.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-f7f5aa4a.js";import"./index-f073b6c8.js";import"./editorWorker-c7aa7ad4.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-639461c9.js";import"./index-bdea1695.js";import"./editorWorker-c7aa7ad4.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.40.0(83b3cf23ca80c94cccca7c5b3e48351b220f8e35)
  * Released under the MIT license
