9a8db3c7b21db6032445d035ee11668de2550d22
Hendrik Käo <hendrik.kao@gmail.com>
2023-09-18 17:47:42 +0200

    Change default value

--- src/default-value.ts
+++ src/default-value.ts
@@ -1,9 +1,21 @@
 const defaultValue = `
-web.events during past 24h
-| where application.name == "Confluence"
-| summarize usage_hours = duration.sum() / 3600 by device.name, device.operating_system.platform, device.entity
-| list device.name, usage_hours, device.operating_system.platform, device.entity
-| sort usage_hours desc
+devices
+
+| with web.errors during past 7d
+
+| compute
+  total_errors_device = number_of_errors.sum()
+
+| where
+  total_errors_device > 10
+
+| list
+  device.name,
+  total_errors_device
+
+| sort
+  total_errors_device
+  desc
 `.trim();
 
 export default defaultValue;
