a67d4f190bb626d7e2d9cd6933a5349b01295866
Hendrik Käo <hendrik.kao@gmail.com>
2023-10-02 08:58:19 +0200

    Remove default value

--- src/default-value.ts
+++ src/default-value.ts
@@ -1,22 +1,3 @@
-const defaultValue = `
-web.page_views
-  during past 7d
-
-| where
-  application.name == "Confluence"
-
-| summarize
-  backendTime = page_load_time.backend.avg()
-  by
-  device.name
-
-| list
-  device.name,
-  backendTime
-
-| sort
-  backendTime
-  desc
-`.trim();
+const defaultValue = ``;
 
 export default defaultValue;
